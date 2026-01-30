4f640287caff81331b12db299297d108f896d9e7
jef70987 <japhethmasinde3@gmail.com>
2026-01-31 00:23:50 +0300

    latest changes

--- school_server/settings.py
+++ school_server/settings.py
@@ -62,7 +62,7 @@ AUTH_USER_MODEL = 'school_app.User'
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
-    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -427,6 +427,7 @@ JAZZMIN_UI_TWEAKS = {
 
 # Static files settings for Render
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
